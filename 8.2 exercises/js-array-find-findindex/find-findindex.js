963e4bfe4a4e5188481c523fef8135545fdde937
Michael Kushman <michaelkushman@MacBook-Pro.local>
2023-05-28 21:47:24 -0700

    completed find, find-index exercise

--- 8.2 exercises/js-array-find-findindex/find-findindex.js
+++ 8.2 exercises/js-array-find-findindex/find-findindex.js
@@ -12,7 +12,12 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(val,i){
+    // console.log(val,val.username)
+    return val.username == username;
+  })
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +32,18 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  // console.log(usersArray,username)
+  let toPop = usersArray.findIndex(function(val,i){
+    return(val.username == username)
+  })
+  // console.log('to pop',usersArray[toPop])
+  if(toPop >= 0){
+    usersArray.pop(usersArray[toPop]);
+    // console.log('to pop',usersArray[toPop])
+    // console.log('users Array',usersArray);
+    return usersArray[toPop];
+}
+    else return undefined;
+  
+}
